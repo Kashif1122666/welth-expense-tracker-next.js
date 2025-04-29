638d6523ea09e311ba06ed269897cdd261885fa9
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2025-04-29 15:27:22 +0500

    add next app with required tools like shadcn and also add basic setup of clerk for authentication

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import { Button } from "../components/ui/button";
+
+
+export default function Home() {
+  return (
+        <>
+         <Button>NO!</Button>
+         <p>hel</p>
+        </>  
+  );
+}
